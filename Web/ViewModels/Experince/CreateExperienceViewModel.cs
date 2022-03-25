e8fd084fd77f5a3301b29f8e2ee0c7ad191dbefe
vilenaNikolaeva <59927349+vilenaNikolaeva@users.noreply.github.com>
2022-03-25 20:09:44 +0200

    change required properties in experience view models.

--- Web/ViewModels/Experince/CreateExperienceViewModel.cs
+++ Web/ViewModels/Experince/CreateExperienceViewModel.cs
@@ -14,13 +14,14 @@ namespace Web.ViewModels
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
         public bool StillWork { get; set; }
-        [Required]
-        [MaxLength(200)]
+        //[Required]
+        //[MaxLength(200)]
         public string JobTitle { get; set; }
-        [Required]
-        [MaxLength(200)]
+        //[Required]
+        //[MaxLength(200)]
         public string CompanyName { get; set; }
         public string Description { get; set; }
+        [Required]
         public string UserId { get; set; }
     }
 }
