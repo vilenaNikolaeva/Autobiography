2d8905fd91f20ae2d8f98410d80ba260804db6ff
vilenaNikolaeva <59927349+vilenaNikolaeva@users.noreply.github.com>
2022-03-28 22:41:30 +0300

    update View Models

--- Web/ViewModels/Education/CreateEducationViewModel.cs
+++ Web/ViewModels/Education/CreateEducationViewModel.cs
@@ -5,7 +5,7 @@ namespace Web.ViewModels
 {
     public class CreateEducationViewModel
     {
-        [Required]
+        //[Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
@@ -13,10 +13,10 @@ namespace Web.ViewModels
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
         public bool Present { get; set; }
-        [Required]
+        //[Required]
         [MaxLength(250)]
         public string University { get; set; }
-        [Required]
+        //[Required]
         [MaxLength(250)]
         public string Title { get; set; }
         public string Description { get; set; }
--- Web/ViewModels/Skill/CreateSkillViewModel.cs
+++ Web/ViewModels/Skill/CreateSkillViewModel.cs
@@ -4,11 +4,11 @@ namespace Web.ViewModels
 {
     public class CreateSkillViewModel
     {
-        [Required(ErrorMessage = "Skill title is required")]
+        //[Required(ErrorMessage = "Skill title is required")]
         [MaxLength(100)]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Skill level is required")]
+        //[Required(ErrorMessage = "Skill level is required")]
         public int Level { get; set; }
         public string UserId { get; set; }
     }
